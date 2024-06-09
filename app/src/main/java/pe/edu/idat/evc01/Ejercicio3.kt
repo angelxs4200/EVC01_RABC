ccffe2989c050e1f53642ae3f11906ce424171ce
angelxs4200 <angelxs5200@gmail.com>
2024-06-09 15:54:40 -0500

    ejercicio 03

--- app/src/main/java/pe/edu/idat/evc01/Ejercicio3.kt
+++ app/src/main/java/pe/edu/idat/evc01/Ejercicio3.kt
@@ -1,6 +1,22 @@
 package pe.edu.idat.evc01
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
 class Ejercicio3 {
 
+    companion object {
+        fun debeRenovarDNI(anioNacimiento: Int): Boolean {
+            val anioActual = 2024
+            val edad = anioActual - anioNacimiento
+            return edad >= 18
+        }
+
+        @Composable
+        fun MostrarResultadoDNI(anioNacimiento: Int) {
+            val debeRenovarDNI = debeRenovarDNI(anioNacimiento)
+            Text(text = "Ejercicio 3: ¿Debe renovar DNI? ${if (debeRenovarDNI) "Sí" else "No"}")
+        }
+    }
 
 }
